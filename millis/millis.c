e39cd422ddd9c471bbedc06016374cdfeec6034f
Kocer <84792756+KocerPL@users.noreply.github.com>
2025-06-30 18:18:41 +0200

    Updated millis library

--- millis/millis.c
+++ millis/millis.c
@@ -1,22 +1,28 @@
-#include "millis.h"
+#include "ch32v003_millis.h"
 uint64_t curMillis =0;
-void Millis_Init()
+void Millis_Init(int16_t periodCorrection)
 {
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure = {0};
     NVIC_InitTypeDef NVIC_InitStructure = {0};
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-    TIM_TimeBaseInitStructure.TIM_Period = 1;
-    TIM_TimeBaseInitStructure.TIM_Prescaler = 11999;
+    
+    TIM_TimeBaseInitStructure.TIM_Period = (SystemCoreClock/1000)-1 +periodCorrection;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 0;
     TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
     TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStructure);
+
     TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
+
     NVIC_InitStructure.NVIC_IRQChannel= TIM2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority =0;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
+
     TIM_Cmd(TIM2,ENABLE);
+
+
 }
 uint64_t millis()
 {
--- millis/millis.h
+++ millis/millis.h
@@ -1,5 +1,9 @@
 #include "ch32v00x_conf.h"
-#define MILLIS_LIB_IN_USE
-void Millis_Init();
+/**
+ * @brief Remember to init millis, otherwise it wont work,
+ * This library uses TIM2 to determine current milliseconds !
+ * @param periodCorrection Period correction, to grow accuracy of millis.
+ */
+void Millis_Init(int16_t periodCorrection);
 uint64_t millis();
 uint64_t seconds();
